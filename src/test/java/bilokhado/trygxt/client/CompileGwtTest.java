fceee9fde1560487280da1df505ec03c37c324cf
Dmytro Bilokha <belohado@rambler.ru>
2016-02-23 11:00:00 +0200

    initial comit

---
+++ src/test/java/bilokhado/trygxt/client/CompileGwtTest.java
@@ -0,0 +1,16 @@
+package bilokhado.trygxt.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class CompileGwtTest extends GWTTestCase {
+  
+  @Override
+  public String getModuleName() {
+    return "bilokhado.trygxt.Project";
+  }
+
+  public void testSandbox() {
+    assertTrue(true);
+  }
+  
+}
