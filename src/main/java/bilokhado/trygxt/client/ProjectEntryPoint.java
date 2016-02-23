56ca49e2177cd2214a26c3d90a29e29651e76c1a
Dmytro Bilokha <belohado@rambler.ru>
2016-02-23 15:19:00 +0200

    Changed buttons

--- src/main/java/bilokhado/trygxt/client/ProjectEntryPoint.java
+++ src/main/java/bilokhado/trygxt/client/ProjectEntryPoint.java
@@ -3,8 +3,13 @@ package bilokhado.trygxt.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.sencha.gxt.core.client.GXT;
+import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
+import com.sencha.gxt.core.client.util.Margins;
 import com.sencha.gxt.widget.core.client.box.MessageBox;
 import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.MarginData;
+import com.sencha.gxt.widget.core.client.container.Viewport;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
@@ -15,16 +20,15 @@ public class ProjectEntryPoint implements EntryPoint {
 
   @Override
   public void onModuleLoad() {
-    String version = GXT.getVersion().getRelease();
-    TextButton textButton = new TextButton("Verify GXT Works: Version=" + version);
-    RootPanel.get().add(textButton);
-    textButton.addSelectHandler(new SelectHandler() {
-      @Override
-      public void onSelect(SelectEvent event) {
-        MessageBox messageBox = new MessageBox("GXT Works.");
-        messageBox.show();
-      }
-    });
+	  FlowLayoutContainer c = new FlowLayoutContainer();
+	    c.setScrollMode(ScrollMode.ALWAYS);
+	    MarginData layoutData = new MarginData(new Margins(0, 5, 0, 0));
+	    c.add(new TextButton("Button A"), layoutData);
+	    c.add(new TextButton("Button B"), layoutData);
+	    c.add(new TextButton("Button C"), layoutData);
+	    Viewport v = new Viewport();
+	    v.add(c);
+	    RootPanel.get().add(v);
   }
   
 }
